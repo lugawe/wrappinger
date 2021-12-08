6e17543ea4aa1832a0d57a9529adda6f30c92129
Luis Weich <weichluis.sw@gmail.com>
2021-12-08 21:51:09 +0100

    WrapperTest update

--- src/test/java/com/github/lugawe/wrappinger/WrapperTest.java
+++ src/test/java/com/github/lugawe/wrappinger/WrapperTest.java
@@ -2,10 +2,42 @@ package com.github.lugawe.wrappinger;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class WrapperTest {
 
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Wrapped {
+    }
+
+    public static class Executor {
+
+        private final UUID id = UUID.randomUUID();
+
+        public Executor() {
+        }
+
+        @Wrapped
+        public String execute() {
+            return id.toString();
+        }
+
+    }
+
     @Test
-    void wrap() {
+    void wrap() throws Exception {
+
+        Executor executor = Wrapper.wrap(Executor.class, Wrapped.class, EmptyHandler.getInstance()).newInstance();
+
+        assertNotNull(executor);
+        assertNotNull(executor.execute());
 
     }
 
