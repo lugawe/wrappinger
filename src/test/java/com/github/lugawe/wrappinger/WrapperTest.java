9df9592a4d5c01d88075369f8e62e9805c9ef740
Luis Weich <weichluis.sw@gmail.com>
2021-12-08 21:55:13 +0100

    WrapperTest updates

--- src/test/java/com/github/lugawe/wrappinger/WrapperTest.java
+++ src/test/java/com/github/lugawe/wrappinger/WrapperTest.java
@@ -6,6 +6,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,6 +40,38 @@ class WrapperTest {
 
         assertNotNull(executor);
         assertNotNull(executor.execute());
+
+        Wrapper.wrap(Executor.class, Wrapped.class, new AbstractHandler<Executor, Wrapped>() {
+
+            @Override
+            public void before() {
+            }
+
+            @Override
+            public void after() {
+            }
+
+            @Override
+            public List<? extends EventListener> getEventListeners() {
+                return Collections.singletonList(new EventListener() {
+
+                    @Override
+                    public void onCompletion(Object result) {
+
+                        assertTrue(result instanceof String);
+
+                        assertEquals(parent.id.toString(), result);
+                    }
+
+                    @Override
+                    public void onException(Exception e) {
+                    }
+
+                });
+            }
+
+        }).newInstance().execute();
+
 
     }
 
