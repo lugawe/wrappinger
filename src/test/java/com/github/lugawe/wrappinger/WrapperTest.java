6d8a630350ec220d724ce318ae901d81303324fb
Luis Weich <weichluis.sw@gmail.com>
2021-12-10 22:23:12 +0100

    WrapperTest use default Wrapped

--- src/test/java/com/github/lugawe/wrappinger/WrapperTest.java
+++ src/test/java/com/github/lugawe/wrappinger/WrapperTest.java
@@ -2,20 +2,11 @@ package com.github.lugawe.wrappinger;
 
 import org.junit.jupiter.api.Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WrapperTest {
-
-    @Target(ElementType.METHOD)
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface Wrapped {
-    }
 
     public static class Executor {
 
@@ -34,7 +25,7 @@ class WrapperTest {
     @Test
     void wrap() throws Exception {
 
-        Executor executor = Wrapper.wrap(Executor.class, Wrapped.class, new AbstractHandler<Executor, Wrapped>() {
+        Executor executor = Wrapper.wrap(Executor.class, new AbstractHandler<Executor, Wrapped>() {
 
             @Override
             public EventListener getEventListener() {
@@ -56,7 +47,7 @@ class WrapperTest {
         assertNotNull(executor);
         assertNotNull(executor.execute());
 
-        Wrapper.wrap(Executor.class, Wrapped.class, new AbstractHandler<Executor, Wrapped>() {
+        Wrapper.wrap(Executor.class, new AbstractHandler<Executor, Wrapped>() {
 
             @Override
             public EventListener getEventListener() {
