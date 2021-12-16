67a41f5fd64e364b22960c5ad8648ffe00a06ed8
Luis Weich <weichluis.sw@gmail.com>
2021-12-16 21:26:21 +0100

    MultiEventListener List instead of Collection

--- src/main/java/com/github/lugawe/wrappinger/MultiEventListener.java
+++ src/main/java/com/github/lugawe/wrappinger/MultiEventListener.java
@@ -3,10 +3,11 @@ package com.github.lugawe.wrappinger;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 public class MultiEventListener implements EventListener {
 
-    protected final Collection<? extends EventListener> eventListeners;
+    protected final List<? extends EventListener> eventListeners;
 
     public MultiEventListener(Collection<? extends EventListener> eventListeners) {
         if (eventListeners == null) {
